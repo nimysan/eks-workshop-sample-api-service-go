2daf6ad82bbcb1aed2013c8b41917c5354e561f3
Sean Ye <234aini@gmail.com>
2022-03-08 16:44:40 +0800

    Update main.go

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World 123123"}
+		res := &response{Message: "Hello World 亲亲啊我的宝贝"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
